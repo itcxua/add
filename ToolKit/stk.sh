b2e596af8b47733f3c3d22e19c45367beb67c533
ITcxUA <105229608+itcxua@users.noreply.github.com>
2024-05-22 20:08:40 +0300

    Update stk.sh

--- ToolKit/stk.sh
+++ ToolKit/stk.sh
@@ -2801,7 +2801,7 @@ function PUREFTP_NEW() {
       read -p "Enter name user: " CREATEUSER;
       sudo mkdir /home/ftpusers/${CREATEUSER};
       sudo pure-pw useradd ${CREATEUSER} -u ftpuser -d /home/ftpusers/${CREATEUSER};
-      pure-pw mkdb; && chown -hR ftpuser:ftpgroup /home/ftpusers/;
+      pure-pw mkdb && chown -hR ftpuser:ftpgroup /home/ftpusers/;
       /etc/init.d/pure-ftpd restart && sleep 3;
 
       sudo pure-pw mkdb;
